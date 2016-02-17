d47da1f8f4079968f5f6c0514bc28c9910067f5a
bencode <bencode@163.com>
2016-02-17 15:40:28 +0800

    test: add test

--- test/index.js
+++ test/index.js
@@ -10,4 +10,39 @@ describe('output-formatter - format string for output', function() {
     fmt.left('hello', 5).should.be.equal('hello');
     fmt.left('hello', 2).should.be.equal('hello');
   });
+
+
+  it('.center(str, len)', function() {
+    fmt.center('hi', 10).should.be.equal('    hi    ');
+    fmt.center('hi', 9).should.be.equal('   hi    ');
+    fmt.center('hi', 2).should.be.equal('hi');
+    fmt.center('hi', 1).should.be.equal('hi');
+  });
+
+
+  it('.right(str, len)', function() {
+    fmt.right('world', 10).should.be.equal('     world');
+    fmt.right('world', 5).should.be.equal('world');
+    fmt.right('world', 1).should.be.equal('world');
+  });
+
+
+  it('.line(ch, num)', function() {
+    fmt.line('-', 5).should.be.equal('-----');
+    fmt.line('+', 0).should.be.equal('');
+  });
+
+
+  it('.red(msg)', function() {
+    const line = fmt.red('hello world');
+    line.should.be.equal('\u001b[31mhello world\u001b[39m');
+    console.log(line);    // eslint-disable-line
+  });
+
+
+  it('.yellow(msg)', function() {
+    const line = fmt.yellow('warnning');
+    line.should.be.equal('\u001b[33mwarnning\u001b[39m');
+    console.log(line);    // eslint-disable-line
+  });
 });
